45a1e0a3f8f22c31ba364580c6cee2635bd59a9d
glorydog <68975076+glorydog@users.noreply.github.com>
2024-10-26 11:23:59 -0400

    Delete empower-calculator directory

--- empower-calculator/src/js/main.js
+++
@@ -1,7 +0,0 @@
-﻿import { Calculator } from './components/Calculator.js';
-import { AdminInterface } from './components/AdminInterface.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const calculator = new Calculator('calculator-container');
-  const adminInterface = new AdminInterface('admin-container');
-});
