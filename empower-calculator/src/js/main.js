594389f47ae8c0d73f6cbdc759f11aa5f1e50463
glorydog <68975076+glorydog@users.noreply.github.com>
2024-10-26 11:14:23 -0400

    Add files via upload

---
+++ empower-calculator/src/js/main.js
@@ -0,0 +1,7 @@
+﻿import { Calculator } from './components/Calculator.js';
+import { AdminInterface } from './components/AdminInterface.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const calculator = new Calculator('calculator-container');
+  const adminInterface = new AdminInterface('admin-container');
+});
